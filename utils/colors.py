2feabd993ee096b9d3380d35d7804db8677f7f85
KuldeepSingh1835 <kuldeep1835singh@gmail.com>
2025-07-28 15:22:16 +0530

    Add files via upload

---
+++ utils/colors.py
@@ -0,0 +1,25 @@
+# utils/colors.py
+
+MEDAL_COLORS = {
+    'Gold': '#FFD700',    # Bright gold (accessible)
+    'Silver': '#A8A8A8',  # Light gray (higher contrast than #C0C0C0)
+    'Bronze': '#CD7F32'   # Original bronze (accessible)
+}
+
+COUNTRY_COLORS = {
+    'USA': '#1B9E77',  # Teal (colorblind-friendly)
+    'IND': '#D95F02',  # Orange
+    'CHN': '#7570B3',  # Purple
+    'JPN': '#E7298A',  # Magenta
+    'GBR': '#66A61E',  # Green
+    'GER': '#E6AB02',  # Yellow
+    'FRA': '#A6761D',  # Brown
+    'RUS': '#666666',  # Gray
+    'BRA': '#1F78B4',  # Blue
+    'AUS': '#B2DF8A',  # Light green
+    'CAN': '#FB9A99',  # Light red
+    'ITA': '#FDBF6F',  # Light orange
+    'KOR': '#CAB2D6',  # Light purple
+    'NED': '#FF7F00',  # Bright orange
+    'ESP': '#6A3D9A'   # Deep purple
+}
